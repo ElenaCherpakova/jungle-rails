25b7fec5afa51f5f88e242d7394df3a9426bf66a
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-02-03 01:59:23 +0000

    count total of products and categories, render it on the dashboard for admin

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"], password: ENV["HTTP_BASIC_PASSWORD"]
 
   def show
+  @count_product=Product.count
+  @count_category=Category.count
   end
 end

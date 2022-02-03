f9d8ac5c05192bf8e887f69502fc5495c41e26d3
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-02-03 22:51:03 +0000

    add authentication, created controlers, cadd signup and login in nav bar

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password_digest, presence: true
+  has_secure_password
+end

fac468bbd14dd7eb29263c8739d9d96bc04494ce
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-02-03 05:28:32 +0000

    rename http_user_name to admin_user_name

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"], password: ENV["HTTP_BASIC_PASSWORD"]
+  http_basic_authenticate_with name: ENV["ADMIN_USER_NAME"], password: ENV["ADMIN_PASSWORD"]
 
   def show
   @count_product=Product.count

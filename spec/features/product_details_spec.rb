9c2adbdc2aacbb0554753c0d899b8b209f1466aa
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-02-08 22:57:33 +0000

    add test for clickingbutton details

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+RSpec.feature "Visitor navigates to home page and clicks on link to details", type: :feature, js: true do
 
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
-    10.times do |n|
+    1.times do |n|
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
@@ -14,20 +14,21 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
         price: 64.99
       )
     end
-    
+
   end
 
-  scenario "They see all products" do
+  scenario "They can navigates from the home page to the product details" do
     # ACT
     visit root_path
 
     # DEBUG
-
-    save_screenshot
-  
+    # save_screenshot
 
     # VERIFY
-    expect(page).to have_css 'article.product', count: 10
-
+    find_link("Details").trigger("click")
+    
+    # page.find('.products').first(:link,'Details').click
+    expect(page).to have_css 'section.products-show'
+    save_screenshot
   end
 end
